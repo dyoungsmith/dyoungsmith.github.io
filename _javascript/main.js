1c8099ad6ea9008de2a0ba589eee9dc87c13d980
Danielle YoungSmith <dyoungsmith@gmail.com>
2021-04-21 20:43:19 -0700

    use hosted fontawesome source

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,7 +1,3 @@
-import "@fortawesome/fontawesome-free/js/brands.js";
-import "@fortawesome/fontawesome-free/js/solid.js";
-import "@fortawesome/fontawesome-free/js/fontawesome.js";
-
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
